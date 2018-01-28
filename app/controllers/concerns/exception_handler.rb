a76a646dda6f89d9c394d0f53642b54121e485c6
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-01-28 10:53:03 -0600

    set up jwt, exceptions, created specs and class files for authorize_api_request

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,13 +1,30 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  # add standard error subclasses
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+
+  # define custom exception handlers
   included do
+    rescue_from ActiveRecord::RecordInvalid, with: :return_422
+    rescue_from ExceptionHandler::AuthenticationError, with: :return_unauthorized
+    rescue_from ExceptionHandler::MissingToken, with: :return_422
+    rescue_from ExceptionHandler::InvalidToken, with: :return_422
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
+  end
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
+  private
+    #returns json response with 422 message
+    def return_422(e)
       json_response({ message: e.message }, :unprocessable_entity)
     end
-  end
+
+    # returns json response with 401 message
+    def return_unauthorized(e)
+      json_response({ message: e.message }, :unauthorized)
+    end
 end
