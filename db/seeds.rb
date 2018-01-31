40ff8d330062baa906ff7a7c0a60bdef1be22cd5
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-01-31 11:48:13 -0600

    created db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create 1 test user
+User.create(
+  first_name: 'Example',
+  last_name: 'User',
+  email: 'example@example.com',
+  password: '123456',
+  password_confirmation: '123456'
+)
+
+# create 3 blogs for test user
+3.times do
+    blog = Blog.create(
+      title: Faker::Book.title,
+      description: Faker::Lorem.sentence,
+      user_id: User.last.id
+    )
+    # for each blog, create 20 posts
+    20.times do
+      blog.posts.create(
+        title: Faker::Lorem.sentence,
+        content: "<h2>My next post</h2><p>#{Faker::Lovecraft.paragraph(20)}</p>",
+        published: Faker::Boolean.boolean(0.9),
+        publish_date: Faker::Date.between(Date.today, 5.days.from_now)
+      )
+    end    
+  end
+
+# Create 4 more random users
+4.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: '123456',
+    password_confirmation: '123456'
+  )
+  # For each user, create 3 blogs
+  3.times do
+    blog = Blog.create(
+      title: Faker::Book.title,
+      description: Faker::Lorem.sentence,
+      user_id: User.last.id
+    )
+    # for each blog, create 20 posts
+    20.times do
+      blog.posts.create(
+        title: Faker::Lorem.sentence,
+        content: "<h2>My next post</h2><p>#{Faker::Lovecraft.paragraph(20)}</p>",
+        published: Faker::Boolean.boolean(0.9),
+        publish_date: Faker::Date.between(Date.today, 5.days.from_now)
+      )
+    end    
+  end
+end
+
+
+
+
