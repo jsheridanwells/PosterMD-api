b79d939d6fc5128d72cf263151821e2646995000
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-01-31 08:32:47 -0600

    basic user model validations pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ApplicationRecord
 
   has_many :blogs, foreign_key: :user_id
 
-  validates_presence_of :first_name, :last_name, :email, :password_digest
+  validates :first_name, :last_name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
+  
+  validates :password, presence: true, length: { minimum: 6 }
+            
+
+
 end
